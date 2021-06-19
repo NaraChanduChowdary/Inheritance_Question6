5b71958bde93ccdd48b7de18f236c1ff46d3b6cb
Chandra Babu Nara <35724636+NaraChanduChowdary@users.noreply.github.com>
2021-06-19 20:51:58 +0100

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Inheritance
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog()
+            {
+                Name = "Jimmy",
+                Age = 2
+            };
+            Console.WriteLine(dog.GetDescription());
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string GetDescription()
+        {
+            return "Name:" + Name + "Age:" + Age.ToString();
+        }
+    }
+    public class Dog : Animal
+    {
+        public int Legs { get; set; }
+    }
+    public class Bird : Animal
+    {
+        public int Wingspan { get; set; }
+    }
+    public class Snake : Animal
+    {
+        public bool IsPoisonous { get; set; }
+    }
+
+}
